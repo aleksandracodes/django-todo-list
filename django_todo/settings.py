c54bfb29887d238fd41ceaa1a3fa59bfd196f4f6
Aleksandra <a.haniok@gmail.com>
2022-03-16 13:17:15 +0000

    Connect to the remote database

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,13 +76,16 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.parse('postgres://atmhpglmqyjdjp:15532d9a66d8bb4c8918c0d20b93666bd89cbf3c74e2dfeb6b3b720021d70a67@ec2-63-35-79-208.eu-west-1.compute.amazonaws.com:5432/daoj9d4mmjmhpn')
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
